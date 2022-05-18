30685fd2b970a91a2c59ec6876ba8e49812dc84d
tubarao312 <p.m.perdigao@gmail.com>
2022-05-18 01:14:32 +0100

    Add comments

--- contracts/HashGameStore.sol
+++ contracts/HashGameStore.sol
@@ -8,61 +8,61 @@ import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 contract HashGameStore {
     // Structs
     struct Developer {
-        string username;
-        address payable walletAddress;
+        string username; // Name of the developer
+        address walletAddress; // Wallet address of the developer
     }
 
     struct Game {
-        string title;
-        uint256 basePrice;
-        Developer developer;
-        uint256 id;
+        string title; // Title of the game
+        uint256 basePrice; // Base price of the game
+        Developer developer; // Developer the game belongs to
+        uint256 id; // ID of the game within all games
     }
 
     struct GameKey {
-        Game game;
-        Player owner;
-        uint256 price;
-        uint256 cooldown;
-        uint256 id;
+        Game game; // The game the key gives access to
+        Player owner; // The player that owns the key
+        uint256 price; // The price it is set to
+        uint256 cooldown; // The date of when the key will be tradable again
+        uint256 id; // The ID within the player's library
     }
 
     struct Player {
         string username; // The player's username
         address payable walletAddress; // The player's wallet address
-        //GameKey[] gameLibrary; // Games the player has in their library
-        //GameKey[] gamesForSale; // Games the player has listed for sale
     }
 
     // Global Variables
-    Player[] private players;
-    Developer[] private developers;
-    Game[] private games;
-    mapping(string => Game) private titleToGame;
-    mapping(string => Player) private usernameToPlayer;
-    mapping(address => Player) private addressToPlayer;
-    mapping(address => Developer) private addressToDeveloper;
-    mapping(address => GameKey[]) private addressToGameLibrary;
-    mapping(address => GameKey[]) private addressToGamesForSale;
-    mapping(string => uint256) public gameTitleToPrice;
+    Game[] private games; // List of all games on the platform
+    mapping(string => Game) private titleToGame; // Maps game title to game struct
+    mapping(string => Player) private usernameToPlayer; // Maps username to player struct
+    mapping(address => Player) private addressToPlayer; // Maps wallet address to player struct
+    mapping(address => Developer) private addressToDeveloper; // Maps wallet address to developer struct
+    mapping(address => GameKey[]) private addressToGameLibrary; // Maps wallet address to game library
+    mapping(address => GameKey[]) private addressToGamesForSale; // Maps wallet address to games for sale
 
+    // The address of our company's wallet
     address payable minter;
 
+    // Constants
     uint256 REVENUE_SHARE = 1; // (1/100) - Percentage that Hash Game Store takes from developers
-    uint256 TRADING_COOLDOWN = 5;
+    uint256 TRADING_COOLDOWN = 5; // Cooldown for game trading
 
     HashToken public token;
     AggregatorV3Interface internal priceFeed;
 
     // Run when contract is deployed
     constructor() public {
-        minter = msg.sender;
-        token = new HashToken(500);
-        priceFeed = AggregatorV3Interface(
+        minter = msg.sender; // Sets the minter as us so we can retrieve the Tokens
+        token = new HashToken(500); // Creates the token contract
+        priceFeed = AggregatorV3Interface( // Price feed to get ETH -> USD conversion
             0x9326BFA02ADD2366b30bacB125260Af641031331
         );
     }
 
+    // Functions ==========================================================================
+
+    // Returns the ETH -> USD price conversion
     function getLatestETHPrice() public view returns (int256) {
         (uint80 a, int256 price, uint256 b, uint256 c, uint80 d) = priceFeed
             .latestRoundData();
@@ -70,40 +70,40 @@ contract HashGameStore {
         return price;
     }
 
-    // Functions
+    // Registers a player with the wallet as the msg.sender
     function playerRegister(string calldata username) public {
         Player memory newPlayer = Player({
             username: username,
             walletAddress: msg.sender
-            //gameLibrary: gameLibrary,
-            //gamesForSale: gamesForSale
         });
 
-        players.push(newPlayer);
         usernameToPlayer[username] = newPlayer;
         addressToPlayer[msg.sender] = newPlayer;
     }
 
+    // Gets the HASH Token Balance of a specific wallet
     function getAddressBalance(address a) public view returns (uint256) {
         return token.balanceOf(a);
     }
 
+    // Gives a player Hash tokens from the contract's wallet
     function givePlayerTokens(string calldata username, uint256 amount) public {
         Player memory player = usernameToPlayer[username];
 
         token.transfer(player.walletAddress, amount);
     }
 
+    // Registers a developer with the wallet as the msg.sender
     function developerRegister(string calldata username) public {
         Developer memory newDeveloper = Developer({
             username: username,
             walletAddress: msg.sender
         });
 
-        developers.push(newDeveloper);
         addressToDeveloper[msg.sender] = newDeveloper;
     }
 
+    // Registers a game with the developer as the msg.sender's developer
     function gameRegister(string calldata title, uint256 price) public {
         Developer memory developer = addressToDeveloper[msg.sender];
 
@@ -115,10 +115,11 @@ contract HashGameStore {
         });
 
         titleToGame[title] = newGame;
-        gameTitleToPrice[title] = price;
+
         games.push(newGame);
     }
 
+    // Generates a new key
     function generateNewKey(Game memory game) private returns (GameKey memory) {
         // Generates a new key
         GameKey memory newKey;
@@ -127,6 +128,7 @@ contract HashGameStore {
         return newKey;
     }
 
+    // Removes a key from an array of keys
     function removeKeyFromArray(GameKey[] storage array, uint256 index)
         private
     {
@@ -139,6 +141,7 @@ contract HashGameStore {
         delete array[array.length - 1];
     }
 
+    // Adds a key to a player's library
     function addKeyToPlayerLibrary(GameKey memory key, Player memory player)
         private
     {
@@ -147,6 +150,7 @@ contract HashGameStore {
         key.id = addressToGameLibrary[player.walletAddress].length;
     }
 
+    // Removes a key from a player's library
     function removeKeyFromPlayerLibrary(GameKey memory key) private {
         // Removes key from player's library
         removeKeyFromArray(
@@ -155,6 +159,7 @@ contract HashGameStore {
         );
     }
 
+    // Buys and generates a new key from a developer as a player
     function buyOriginalKey(string memory gameTitle) public {
         Player memory player = addressToPlayer[msg.sender];
         Game memory game = titleToGame[gameTitle];
@@ -190,6 +195,7 @@ contract HashGameStore {
         newKey.cooldown = now + 5 minutes;
     }
 
+    // Gets the price of a key from a player's sale library
     function getKeyPrice(string calldata sellerUsername, uint256 keyID)
         public
         view
@@ -201,14 +207,17 @@ contract HashGameStore {
             ][keyID].price;
     }
 
+    // Gets the base price of a game
     function getGamePrice(string calldata title) public view returns (uint256) {
         return titleToGame[title].basePrice;
     }
 
+    // Gets the game title of a game based on its ID
     function getGameTitle(uint256 id) public view returns (string memory) {
         return games[id].title;
     }
 
+    // Gets an array with the IDs of all the games a player owns
     function getPlayerLibrary(string calldata username)
         public
         view
@@ -229,6 +238,7 @@ contract HashGameStore {
         return gameIDArray;
     }
 
+    // Returns the contract address of the token
     function getToken() public view returns (HashToken) {
         return token;
     }
